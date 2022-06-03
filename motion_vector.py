ca96ec3f73af0c25ae4d6c2e3dc1949d37027a7f
Jaemin.Park <once29@naver.com>
2022-06-04 00:03:09 +0900

    Fix motion_vector.py to change WINDOW_SIZE

--- motion_vector.py
+++ motion_vector.py
@@ -1,66 +1,52 @@
 import cv2
 import numpy as np
+import numpy.linalg
 
-""" param 조작 ↓"""
-N_SIZE = 7
-GAUSSIAN_KSIZE = 7
-DERIVATIVE_X = [[-0.5, 0, 0.5]]
-DERIVATIVE_Y = [[-0.5],
-                [0],
-                [0.5]]
 
-# 이미지를 불러온다.
-color_first_img = cv2.imread("img/img0.png", 1)
-color_second_img = cv2.imread("img/img1.png", 1)
-first_img = cv2.imread("img/img0.png", 0)
-second_img = cv2.imread("img/img1.png", 0)
+WINDOW_SIZE = 9
+DERIVATIVE_FILTER_X = [[-1, 0, 1]]
+DERIVATIVE_FILTER_Y = [[-1], [0], [1]]
+t0_image = cv2.imread("img/img0.png", 1)
+t0_image_grey = cv2.imread("img/img0.png", 0)
+t1_image_grey = cv2.imread("img/img1.png", 0)
 
-# df/dy, df/dx, df/dt를 찾는다.
-height, width = first_img.shape
-df_dy = cv2.filter2D(first_img, ddepth=cv2.CV_64F, kernel=np.array(DERIVATIVE_Y))
-df_dx = cv2.filter2D(first_img, ddepth=cv2.CV_64F, kernel=np.array(DERIVATIVE_X))
-df_dt = np.zeros((height, width))
+height, width, _ = t0_image.shape
+dx = cv2.filter2D(t0_image_grey, kernel=np.array(DERIVATIVE_FILTER_X), ddepth=cv2.CV_64F)
+dy = cv2.filter2D(t0_image_grey, kernel=np.array(DERIVATIVE_FILTER_Y), ddepth=cv2.CV_64F)
+dt = np.zeros((height, width))
+
 for y in range(height):
     for x in range(width):
-        df_dt[y][x] = float(second_img[y][x]) - float(first_img[y][x])
+        dt[y][x] = float(t1_image_grey[y][x]) - float(t0_image_grey[y][x])
+N = int(WINDOW_SIZE / 2)
+result = np.zeros(shape=(height, width, 2))
+for y in range(N, height - N):
+    for x in range(N, width - N):
+        derivative_y = dy[(y - N):(y + N + 1), (x - N):(x + N + 1)]
+        derivative_x = dx[(y - N):(y + N + 1), (x - N):(x + N + 1)]
+        derivative_t = dt[(y - N):(y + N + 1), (x - N):(x + N + 1)]
 
-# A, b를 구성하고, v를 구한다.
-n = int(N_SIZE / 2)
-result = np.empty(shape=(height, width, 2))
-for y in range(n, height - n):
-    for x in range(n, width - n):
+        A = np.concatenate(
+            [np.reshape(derivative_y, (WINDOW_SIZE ** 2, 1)), np.reshape(derivative_x, (WINDOW_SIZE ** 2, 1))],
+            axis=-1
+        )
+        b = -np.reshape(derivative_t, (WINDOW_SIZE ** 2, 1))
+
+        A_TA = np.matmul(A.T, A)
+        A_TB = np.matmul(A.T, b)
         try:
-            # N 범위에 있는 픽셀들을 모아준다.
-            y_tmp = df_dy[(y - n):(y + n + 1), (x - n):(x + n + 1)]
-            x_tmp = df_dx[(y - n):(y + n + 1), (x - n):(x + n + 1)]
-            t_tmp = df_dt[(y - n):(y + n + 1), (x - n):(x + n + 1)]
-
-            # A, v, b 행렬을 구성한다.
-            A = np.array(cv2.hconcat([np.reshape(y_tmp, (N_SIZE ** 2, 1)), np.reshape(x_tmp, (N_SIZE ** 2, 1))]))
-            A_T = A.T
-            b = -np.reshape(t_tmp, (N_SIZE ** 2, 1))
-
-            # 계산해 v_T를 구한다.
-            mat1 = np.matmul(A_T, A)
-            mat2 = np.matmul(A_T, b)
-            v_T = np.matmul(np.linalg.inv(mat1), mat2)
-            # result[y][x]에 저장한다.
+            v_T = np.matmul(np.linalg.inv(A_TA), A_TB)
             result[y][x] = v_T.T
         except:
-            # 역행렬이 없는 경우, 예외처리한다.
             result[y][x] = np.array([0., 0.])
 
-# 이미지에 화살표를 그려준다.
-background = cv2.addWeighted(color_first_img, 0.8, color_second_img, 0.2, 0)
-for y in range(n, height - n, n):
-    for x in range(n, width - n, n):
+for y in range(N, height - N, N):
+    for x in range(N, width - N, N):
         v, u = result[y][x]
-        start_point = (x, y)
-        end_point = (x + int(u), y + int(v))
-        if start_point != end_point:
-            background = cv2.arrowedLine(img=background, pt1=start_point, pt2=end_point,
-                                         color=(0, 0, 255), thickness=1, tipLength=0.1)
-
-# 이미지를 보여준다.
-cv2.imshow('result', background)
+        source = (x, y)
+        destination = (x + int(u), y + int(v))
+        if source != destination:
+            background = cv2.arrowedLine(img=t0_image, pt1=source, pt2=destination,
+                                         color=(0, 0, 255), tipLength=0.1)
+cv2.imshow('result', t0_image)
 cv2.waitKey()
